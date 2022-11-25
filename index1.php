4ad9ac70c2e2bbba7f1b855fc5c98138aa561cb1
Santosh-Tripathi <108662593+Santosh-Tripathi@users.noreply.github.com>
2022-11-25 23:03:39 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Investor Page</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+<style>
+    :root{
+    --green:#E8E8E8;
+    --black:#444;
+    --light-color:#777;
+    --gray:#E8E8E8;
+    --border:.2rem solid var(--green);
+}
+    .icons-container{
+    display: grid;
+    gap:5rem;
+    grid-template-columns: repeat(auto-fit, minmax(12rem, 1fr));
+    padding-top: 5rem;
+    padding-bottom: 5rem;
+}
+
+.icons-container .icons{
+    border:var(--border);
+    box-shadow: var(--box-shadow);
+    border-radius: .5rem;
+    text-align: center;
+    padding: 2.5rem;
+}
+
+.icons-container .icons i{
+    font-size: .5rem;
+    color:var(--green);
+    padding-bottom: .7rem;
+}
+
+.icons-container .icons h3{
+    font-size: 1.7rem;
+    color:var(--black);
+    padding: .5rem 0;
+    text-shadow: var(--text-shadow);
+}
+
+.icons-container .icons p{
+    font-size: 1rem;
+    color:var(--light-color);
+}
+
+    </style>
+</head>
+<body class="bg-light">
+
+    <div class="container col-md-12">
+        <div class="row">
+            <div class="col-md-6">
+                <h3>Invest in ChurchSpace</h3><br><br>
+                <h2>Serving a +7B industry <br>that  has not seen<br> innovation in decades</h2>
+                <button type="submit" class="btn text-white bg-dark shadow-none">Invest Today</button>
+            </div>
+            <div class="col-md-6">
+                <img src="images/1.jpg" width="100%" class="mb-5">
+            </div>
+
+        </div>
+    </div>
+    <!-- llllllllllllllllllllllllllllllll -->
+    
+  
+            <div class="bg-dark align-items-center">
+                <div class="mx-auto">
+                    <h6 class="text-white mt-5">As Seen In</h6>
+                </div>
+                <img src="images/2.jpg" class="mt-5 mb-5  mx-4">
+            </div>
+        
+     <!-- l -->
+
+        <div class="container mt-5">
+        <div class="row">
+            <div class="col-lg-12 bg-light">
+                <h6>Company Highlights</h6>
+            </div>
+        </div>
+        <section class="icons-container">
+
+        <div class="icons">
+        <h3>1200+</h3>
+        <p>Pending Church Partnerships</p>
+    </div>
+
+    <div class="icons">
+        <h3>6K+</h3>
+        <p>'Ready-to-Book' geust sign ups</p>
+    </div>
+
+    <div class="icons">
+        <h3>97%</h3>
+        <p>Guest return rate (pilot year user data)</p>
+    </div>
+
+    <div class="icons">
+        <h3>173%</h3>
+        <p>Revenue growth(Q4 2019-Q4 2021)</p>
+    </div>
+
+    <div class="icons">
+        <h3>Successfully Sold</h3>
+        <p>ChurchSpace Tv(covid-19 pivot business)</p>
+    </div>
+
+    <div class="icons">
+        <h3>1000+ hrs</h3><br>
+        <p>User Research</p>
+    </div>
+    </section>
+</div>
+
+<div class="container bg-dark ms-0">
+            <div class="col-lg-12">
+                <h5 class="text-white mt-3 ms-4">About</h5>
+                <p class="text-white ms-5 mt-5 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore numquam reiciendis eligendi nam illum repellendus et sequi quos exercitationem voluptates eum odio id accusantium, tempora officiis deserunt architecto minima harum!</p>
+            </div>
+</div>
+
+<div class="container">
+        <h4>How ChurchSpace works</h4>
+    <div class="col-md-6">
+                <img src="images/3.jpg" width="100%" class="mb-5">
+    </div>  
+</div>
+
+<div class="container bg-dark text-white col-md-12">
+    <div class="row">
+        <div class="col-mb-6">
+             <h3 class="ms-4">Testimonials|Supporting Small Businesses</h3><br>
+             <h3 class="ms-4">Julia Hogan-McNeil,Julia Cooks Organic Food</h3>
+            <h6 class="ms-4">ChurchSpace Guest</h6><br><br>
+            <h6 class="ms-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima qui earum facere libero maiores. Laborum repellendus deleniti atque, porro possimus consequatur ratione, fugiat quis, est rem unde? Quo, deleniti sint!</h6>
+    </div>
+    <div class="col md-12">
+        <img src="images/4.jpg" width="100%">
+    </div>
+</div>
+
+</div>
+</body>
+</html>
